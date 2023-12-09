ff1105ce68a56f9dfbe0148f1131cf7be7f05889
Power70 <nwisuanu@gmail.com>
2023-12-09 20:30:45 +0100

    implent another feature of using useFetcher to fetch data from the menu without neccessarily navigating to that page. also fixed a bug

--- fast-react-pizza/src/features/order/Order.jsx
+++ fast-react-pizza/src/features/order/Order.jsx
@@ -1,48 +1,25 @@
 // Test ID: IIDSAT
+import { useFetcher, useLoaderData } from "react-router-dom";
 import OrderItem from "./OrderItem"
-import { useLoaderData } from "react-router-dom";
 import { getOrder } from "../../services/apiRestaurant";
 import { calcMinutesLeft, formatCurrency, formatDate } from "../../ultilities/helpers";
+import { useEffect } from "react";
 
 
 
-// const order = {
-//   id: "ABCDEF",
-//   customer: "Jonas",
-//   phone: "123456789",
-//   address: "Arroios, Lisbon , Portugal",
-//   priority: true,
-//   estimatedDelivery: "2027-04-25T10:00:00",
-//   cart: [
-//     {
-//       pizzaId: 7,
-//       name: "Napoli",
-//       quantity: 3,
-//       unitPrice: 16,
-//       totalPrice: 48,
-//     },
-//     {
-//       pizzaId: 5,
-//       name: "Diavola",
-//       quantity: 2,
-//       unitPrice: 16,
-//       totalPrice: 32,
-//     },
-//     {
-//       pizzaId: 3,
-//       name: "Romana",
-//       quantity: 1,
-//       unitPrice: 15,
-//       totalPrice: 15,
-//     },
-//   ],
-//   position: "-9.000,38.000",
-//   orderPrice: 95,
-//   priorityPrice: 19,
-// };
 
 function Order() {
   const order = useLoaderData();
+  // if you want to fetch something in another route without using usenavigate 
+  // we make use of the useFetcher hook
+  const fetcher = useFetcher();
+
+  // to use the data as the component mount 
+  // we use the use effect hook and call our fetcher on inside it
+  //  then on our fetcher we call the load and pass the route into it
+  useEffect(function(){
+    if(!fetcher.data && fetcher.state === "idle")fetcher.load("/menu")
+  }, [fetcher]);
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
     id,
@@ -76,7 +53,16 @@ function Order() {
       </div>
 
       <ul className="divide-y divide-stone-200 border-b border-t">
-        {cart.map((item) => (<OrderItem item={item} key={item.id} />))}
+        {cart.map((item) => 
+          (<OrderItem 
+          item={item} 
+          key={item.pizzaId}
+          isLoadingIngredients={fetcher.state === "loading"}
+          ingredients={
+            fetcher?.data?.find((el) => el.id === item.pizzaId)?.ingredients ?? []
+          }
+          />
+        ))}
       </ul>
 
       <div className="space-y-2 bg-stone-200 px-6 py-5">
