d20b4c94203781506bec2b60aaa168ce72f370a9
Power70 <nwisuanu@gmail.com>
2023-12-23 14:28:26 +0100

    handle error on form and created a new reuseable component

--- the-wild-oasis/src/ui/AppLayout.jsx
+++ the-wild-oasis/src/ui/AppLayout.jsx
@@ -9,11 +9,11 @@ grid-template-columns: 26rem 1fr;
 height: 100vh;
 grid-template-rows: auto 1fr;
 `;
+
 const Main = styled.main`
 background-color: var(--color-grey-50);
 padding: 4rem 4.8rem 6.4rem;
-
-
+overflow: scroll;
 `;
 
 const Container = styled.div`
